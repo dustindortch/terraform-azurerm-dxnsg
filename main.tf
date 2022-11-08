701bfa9172fa95552bb08730474dae9aff6265aa
Dustin Dortch <me@dustindoertch.com>
2022-11-08 11:26:44 -0500

    Fix conflicts between address range/ranges and port range/ranges

--- main.tf
+++ main.tf
@@ -16,17 +16,21 @@ resource "azurerm_network_security_rule" "rules" {
   name                                       = each.key
   access                                     = each.value.access
   description                                = each.value.description
-  destination_address_prefixes               = each.value.destination_address_prefixes
+  destination_address_prefix                 = length(each.value.destination_address_prefixes) == 0 ? each.value.destination_address_prefix : null
+  destination_address_prefixes               = length(each.value.destination_address_prefixes) == 0 ? null : each.value.destination_address_prefixes
   destination_application_security_group_ids = each.value.destination_application_security_group_ids
-  destination_port_ranges                    = each.value.destination_port_ranges
+  destination_port_range                     = length(each.value.destination_port_ranges) == 0 ? each.value.destination_port_range : null
+  destination_port_ranges                    = length(each.value.destination_port_ranges) == 0 ? null : each.value.destination_port_ranges
   direction                                  = each.value.direction
   network_security_group_name                = azurerm_network_security_group.nsg.name
   priority                                   = each.value.priority
   protocol                                   = each.value.protocol
   resource_group_name                        = azurerm_network_security_group.nsg.resource_group_name
-  source_address_prefixes                    = each.value.source_address_prefixes
+  source_address_prefix                      = length(each.value.source_address_prefixes) == 0 ? each.value.source_address_prefix : null
+  source_address_prefixes                    = length(each.value.source_address_prefixes) == 0 ? null : each.value.source_address_prefixes
   source_application_security_group_ids      = each.value.source_application_security_group_ids
-  source_port_ranges                         = each.value.source_port_ranges
+  source_port_range                          = length(each.value.source_port_ranges) == 0 ? each.value.source_port_range : null
+  source_port_ranges                         = length(each.value.source_port_ranges) == 0 ? null : each.value.source_port_ranges
 }
 
 resource "azurerm_subnet_network_security_group_association" "assoc" {
--- variables.tf
+++ variables.tf
@@ -25,18 +25,24 @@ variable "rules" {
   type = map(object({
     access                                     = string
     description                                = optional(string)
-    destination_address_prefixes               = list(string)
-    destination_application_security_group_ids = optional(list(string), [])
-    destination_port_ranges                    = optional(list(string), ["*"])
+    destination_address_prefix                 = optional(string, "*")
+    destination_address_prefixes               = optional(list(string), [])
+    destination_application_security_group_ids = optional(list(string))
+    destination_port_range                     = optional(string)
+    destination_port_ranges                    = optional(list(string), [])
     direction                                  = string
     priority                                   = number
     protocol                                   = optional(string, "*")
-    source_address_prefixes                    = optional(list(string), ["*"])
-    source_application_security_group_ids      = optional(list(string), ["*"])
-    source_port_ranges                         = optional(list(string), ["*"])
+    source_address_prefix                      = optional(string, "*")
+    source_address_prefixes                    = optional(list(string), [])
+    source_application_security_group_ids      = optional(list(string))
+    source_port_range                          = optional(string, "*")
+    source_port_ranges                         = optional(list(string), [])
   }))
 
   description = "Set of rules to include in network security group."
+
+  default = {}
 }
 
 variable "subnet_ids" {
